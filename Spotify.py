b906f8623d5d58e0fefac5249b0a937366b0ae32
Luis Losada <47015005+luislo92@users.noreply.github.com>
2020-01-10 12:02:37 -0500

    Update Spotify.py

--- Spotify.py
+++ Spotify.py
@@ -9,12 +9,11 @@ import sys
 import pprint
 import os
 
-os.chdir('/Users/luislosada/Columbia Drive/Applied Text and NLP')
+os.chdir('<Set Up your working Directory')
 
 ##Set up your client ID and Client Secret
-CLIENT_ID = "33a602d7e56540d3af86373bcba237af"
-CLIENT_SECRET = "6d36561d42694d8c8747079b156e15cb"
-
+CLIENT_ID = <YOUR CLIENT ID>
+CLIENT_SECRET = <YOUR CLIENT SECRET>
 credentials = oauth2.SpotifyClientCredentials(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET)
@@ -26,7 +25,8 @@ uri = 'spotify:user:spotifycharts:playlist:37i9dQZEVXbMDoHDwVN2tF?si=0MkpydG_SWC
 username = uri.split(':')[2]
 playlist_id = uri.split(':')[4]
 
-#print(json.dumps(results, indent=4))
+# This is a selection of 25 popular Spotify playlists, to get the id of different playlists just use the Spotify web app
+# the last part of the url has the playlist ID.
 
 list_of_playslists = ["37i9dQZF1DX0XUsuxWHRQd?si=BO56978gQqSiLl4g73P4jA","37i9dQZEVXbMDoHDwVN2tF?si=0MkpydG_SWC5Y7loG59M9Q",
 "37i9dQZF1DX0MLFaUdXnjA","37i9dQZF1DWXJyjYpHunCf","37i9dQZF1DWWOrEmtp56BZ","37i9dQZF1DWSkMjlBZAZ07",
@@ -57,12 +57,14 @@ def df_creation(playlist_id):
             df_t50.loc[i, 'popularity'] = None
             df_t50.loc[i, 'playlist'] = None
     return df_t50
-Songs_Df = {}
+
+#Creating a dictionary of datasets for each playlist
+Songs_Df = {} 
 for playll in list_of_playslists:
     Songs_Df[playll] = df_creation(playll)
 
+#Merging the Dictionary into one big dataset    
 df = pd.concat(Songs_Df).reset_index(drop=True)
-
 df = df.mask(df.astype(object).eq('None')).dropna().reset_index(drop=True)
 
 df2 = df.drop(columns=["playlist"]).drop_duplicates().reset_index()
@@ -70,7 +72,7 @@ df2 = df.drop(columns=["playlist"]).drop_duplicates().reset_index()
 df3 = pd.merge(df2,df['playlist'],how='left',left_on=df2['index'],right_index=True).drop(columns=['index'])
 
 #Pulling Lyrics
-api = genius.Genius('N9fjx5mBa9dVc6dx6fxonQ8RdH57gGZp8wCWCCzTNXa_yr6qSksEL8GBzLXKRizf')
+api = genius.Genius('<GENIUS API CODE>')
 
 lyrics = []
 for a in range(len(df3)):
@@ -80,9 +82,10 @@ for a in range(len(df3)):
     except AttributeError:
         lyrics.append(" ")
 
-
+#Merging Lyrics to Dataset
 df3.insert(loc=5,column = 'lyrics',value = lyrics)
 df4 = df3.mask(df3.astype(object).eq('None')).dropna().reset_index(drop=True)
-df4.to_csv('pre-clean-dataset.csv')
 
+#Create CSV to use later on
+df4.to_csv('pre-clean-dataset.csv')
 
